ae12e75d50012d0339ab50c9fb39e5a2fdf809be
John Velis <john@johnvelis.com>
2017-01-27 17:10:30 -0500

    initial commit

---
+++ CA_ATaleOfThreeCalculators/Controllers/UrlCalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CA_ATaleOfThreeCalculators.Controllers
+{
+    public class UrlCalculatorController : Controller
+    {
+        // GET: UrlCalculator
+        public ActionResult Add(double operand1, double operand2)
+        {
+            if (operand1 != null && operand2 != null)
+            {
+                ViewBag.answer = (operand1 + operand2).ToString();
+            }
+            else
+            {
+                ViewBag.errorMessage = "You did not enter valid numbers.";
+            }
+
+            return View();
+        }
+    }
+}
