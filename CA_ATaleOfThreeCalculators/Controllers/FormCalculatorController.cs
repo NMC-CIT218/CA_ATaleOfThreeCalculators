6bc2910112b1ad3d7b06916f6800335e7900456a
John Velis <john@johnvelis.com>
2017-01-28 07:22:02 -0500

    submit721

--- CA_ATaleOfThreeCalculators/Controllers/FormCalculatorController.cs
+++ CA_ATaleOfThreeCalculators/Controllers/FormCalculatorController.cs
@@ -8,9 +8,67 @@ namespace CA_ATaleOfThreeCalculators.Controllers
 {
     public class FormCalculatorController : Controller
     {
-        // GET: FormCalculator
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Calculate(string operation, string operand1, string operand2)
+        {
+            double operand1Num;
+            double operand2Num;
+            double answerNum = 0;
+            List<string> errorMessageText = new List<string>();
+            ViewBag.errorMessage = null;
+            ViewBag.answer = null;
+
+            if ((double.TryParse(operand1, out operand1Num)) && (double.TryParse(operand2, out operand2Num)))
+            {
+                switch (operation)
+                {
+                    case "add":
+                        {
+                            answerNum = operand1Num + operand2Num;
+                            break;
+                        }
+                    case "subtract":
+                        {
+                            answerNum = operand1Num - operand2Num;
+                            break;
+                        }
+                    case "multiply":
+                        {
+                            answerNum = operand1Num * operand2Num;
+                            break;
+                        }
+                    case "divide":
+                        {
+                            if (operand2Num != 0)
+                            {
+                                answerNum = operand1Num / operand2Num;
+                            }
+                            else
+                            {
+                                errorMessageText.Add("It appears that you are dividing by zero.");
+                                errorMessageText.Add("Please click the Try Again button below to reenter your numbers.");
+                            }
+                            break;
+                        }
+                    default:
+                        errorMessageText.Add("It appears that you did not enter valid operation.");
+                        errorMessageText.Add("Please click the Try Again button below to reenter your numbers.");
+                        break;
+                }
+                ViewBag.answer = answerNum;
+            }
+            else
+            {
+                errorMessageText.Add("It appears that you did not enter valid numbers.");
+                errorMessageText.Add("Please click the Try Again button below to reenter your numbers.");
+            }
+
+            ViewBag.errorMessage = errorMessageText;
+
             return View();
         }
     }
